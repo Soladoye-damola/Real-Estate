23d3af6bdc78ad09d6248eaa661be969a2fc9b1b
Soladoye-damola <abdullateefsoladoye@gmail.com>
2025-11-04 07:14:39 -0800

    create and run server

---
+++ client-side/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import  {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to="/"><h1 className='text-purple-500 text-4xl font-bold text-sm sm:text-xl'>Ìpàdé</h1></Link>
+        <form className='bg-slate-100 rounded-lg flex items-center p-2'>
+            <input type='text' className='bf-transparent focus:outline-none w-24 sm:w-64' placeholder='Search...'/>
+            <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline cursor:pointer'>Home</li></Link>
+            <Link to='about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline cursor:pointer'>About</li></Link>
+            <Link to='sign-in'>
+            <li className='text-slate-700 hover:underline cursor:pointer'>Sign In</li></Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
