b20701f274ca0446d3465b7b144af59291519881
Soladoye-damola <abdullateefsoladoye@gmail.com>
2025-11-04 05:39:07 -0800

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import SignIn from './pages/Sign-In'
 import SignOut from './pages/Sign-Out'
 import ProfilePage from './pages/Profile-Page'
 import AboutPage from './pages/About-Page'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
+      
       <Route path='' element={<HomePage/>}/>
       <Route path='sign-up' element={<SignUp/>}/>
       <Route path='sign-in' element={<SignIn/>}/>
--- src/pages/About-Page.jsx
+++ src/pages/About-Page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div></div>
+    <div>About</div>
   )
 }
