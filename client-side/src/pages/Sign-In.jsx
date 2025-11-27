e7d8fdbe4b0f24124882f29a9d0467796c6fb299
Soladoye-damola <abdullateefsoladoye@gmail.com>
2025-11-26 21:29:52 -0800

    complete sign in functionality

--- client-side/src/pages/Sign-In.jsx
+++ client-side/src/pages/Sign-In.jsx
@@ -1,43 +1,61 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, useNavigate} from 'react-router-dom'
 import { useState } from 'react'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] =useState(false);
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
+  const handleChange = (e)=>{
     setFormData({
-      ...formData, [e.target.id]: e.target.value
+      ...formData,
+      [e.target.id]: e.target.value
     });
-  };
-
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-    const res = await fetch('/api/auth/signup', {
-      method: "POST",
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(formData),
-    });
-   const data = await res.json();
-   console.log(data);
-
   }
+
+  const handleSubmit = async (e)=>{
+  e.preventDefault();
+  try{
+  setLoading(true);
+  const res =  await fetch('/api/auth/signin',
+  {
+   method:'POST',
+   headers:{
+           'content-type': 'application/json'
+   },
+    body:JSON.stringify(formData) 
+  }
+  );
+  const data = await res.json();
+  if (data.success === false){
+    setError(data.message);
+    setLoading(false)
+    return;
+  }
+  setError(null);
+  setLoading(false);
+  navigate('/');
+  }catch(error){
+    setLoading(false);
+    setError(error.message);
+  }
+}
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className=' flex flex-col gap-4'>
-        <input type="text" placeholder='Username' id='username' className='border p-3 rounded-lg' onChange={handleChange} />
+        <input type="email" placeholder='Email' id='email' className='border p-3 rounded-lg' onChange={handleChange} />
         <input type="password" placeholder='Password' id='password' className='border p-3 rounded-lg' onChange={handleChange} />
-        <button className='bg-slate-700 text-white border rounded-lg p-3 hover:opacity-80 disabled:opacity-80 cursor-pointer'>SIGN IN</button>
+        <button disabled={loading} className='bg-slate-700 text-white border rounded-lg p-3 hover:opacity-80 disabled:opacity-80 cursor-pointer'> {loading ? 'LOADING...' : 'SIGN IN'}</button>
       </form>
       <div className='flex gap-2 m'>
-        <p className='font-semibold'>New to Ìpàdé?</p>
-        <Link to={"/sign-up"}><span className='text-blue-700'>Create account</span></Link>
-
+        <p className='font-semibold'>Don't have an account?</p>
+        <Link to={"/sign-up"}><span className='text-blue-700'>Sign In</span></Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
--- client-side/src/pages/Sign-Up.jsx
+++ client-side/src/pages/Sign-Up.jsx
@@ -1,57 +1,54 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router'
+import { Link, redirectDocument, useNavigate } from 'react-router'
 import { useState } from 'react'
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] =useState({});
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
+  const [loading, setLoading] =useState(false);
   const navigate = useNavigate();
 
-
-  const handleChange = (e) => {
+  const handleChange = (e)=>{
     setFormData({
-      ...formData, [e.target.id]: e.target.value
+      ...formData,
+      [e.target.id]: e.target.value
     });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const res = await fetch('/api/auth/signup', {
-        method: "POST",
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      console.log(data);
-      if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-
-
-        return;
-      }
-      setLoading(false);
-      setError(null);
-      navigate('/sign-in');
-
-    } catch (error) {
-      setLoading(false)
-      setError(error.message);
-    }
   }
+
+  const handleSubmit = async (e)=>{
+  e.preventDefault();
+  try{
+  setLoading(true);
+  const res =  await fetch('/api/auth/signup',
+  {
+   method:'POST',
+   headers:{
+           'content-type': 'application/json'
+   },
+    body:JSON.stringify(formData)
+  }
+  );
+  const data = await res.json();
+  if (data.success === false){
+    setError(data.message);
+    setLoading(false)
+    return;
+  }
+  setError(null);
+  setLoading(false);
+  navigate('/sign-in');
+  }catch(error){
+    setLoading(false);
+    setError(error.message);
+  }
+}
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className=' flex flex-col gap-4'>
         <input type="text" placeholder='Username' id='username' className='border p-3 rounded-lg' onChange={handleChange} />
-        <input type="text" placeholder='Email' id='email' className='border p-3 rounded-lg' onChange={handleChange} />
+        <input type="email" placeholder='Email' id='email' className='border p-3 rounded-lg' onChange={handleChange} />
         <input type="password" placeholder='Password' id='password' className='border p-3 rounded-lg' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white border rounded-lg p-3 hover:opacity-80 disabled:opacity-80 cursor-pointer'> {loading ? 'LOADING...' : 'SIGN UP'}</button>
       </form>
