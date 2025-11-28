e9ccec92f72851e8f50b6367a0dbc1be3af0fe71
Soladoye-damola <abdullateefsoladoye@gmail.com>
2025-11-27 18:14:13 -0800

    redux toolkit

---
+++ client-side/src/redux/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+import { signin } from "../../../api/controllers/auth.controller";
+
+
+
+const initialState = {
+    currentUser: null,
+    loading:false,
+    error:null
+}
+
+const userSlice = createSlice({
+  name:'user',
+  initialState,
+  reducers:{
+   signInStart:(state)=>{
+    state.loading = true;
+   },
+
+   signInSuccess:(state, action){
+    state.currentUser = action.payload,
+    state.loading = false,
+    state.error = null,
+   },
+   signInFailure:(state, action){
+    state.error = action.payload,
+    state.loading = false
+   }
+  }
+});
+
+export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+
+export default userSlice.reducer;
